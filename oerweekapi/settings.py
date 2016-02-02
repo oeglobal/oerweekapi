879f1f92b406e7ca497ca1f321085937fb5d7acb
Jure Cuhalev <gandalfar@gmail.com>
2016-02-02 15:29:45 +0100

    pagination

--- oerweekapi/settings.py
+++ oerweekapi/settings.py
@@ -107,6 +107,11 @@ REST_FRAMEWORK = {
     ),
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 9
+}
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 JWT_AUTH = {
