a174153d4f90f986c9d35148ed6acef7404c48ee
Jure Cuhalev <gandalfar@gmail.com>
2016-08-17 16:18:46 +0200

    Debug view

--- web/views.py
+++ web/views.py
@@ -15,7 +15,7 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
 
 from .importer import import_resource, import_openphoto, import_submission
@@ -36,11 +36,13 @@ class CustomResultsSetPagination(PageNumberPagination):
 
 
 class OpenPhotoViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    # permission_classes = (IsAuthenticatedOrReadOnly,)
+    # permission_classes = [IsAuthenticated,]
     queryset = OpenPhoto.objects.filter(post_status='publish',).order_by('?')
     pagination_class = LargeResultsSetPagination
 
     def get_serializer_class(self):
+        print(self.request.user.is_authenticated())
         if self.request.method in ['PUT', 'POST'] and self.request.user.is_authenticated():
             return AuthenticatedOpenPhotoSerializer
 
