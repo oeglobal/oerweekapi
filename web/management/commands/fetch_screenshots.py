adcc89c18944bc2f6bdf2d4bb5a6989557534f20
Jure Cuhalev <jure@cuhalev.com>
2017-02-09 13:29:35 +0100

    Fix queryset

--- web/management/commands/fetch_screenshots.py
+++ web/management/commands/fetch_screenshots.py
@@ -6,6 +6,6 @@ class Command(BaseCommand):
     help = "Exports emails"
 
     def handle(self, *args, **options):
-        for resource in Resource.objects.filter(screenshot_status='', created__year=2017, status__in=['draft', 'publish']):
+        for resource in Resource.objects.filter(screenshot_status='', created__year=2017, post_status__in=['draft', 'publish']):
             print(resource.id)
             resource.get_screenshot()
