25c00b56c64791f6011b5c82dbe2f811a1517518
Jure Cuhalev <jure@cuhalev.com>
2018-08-08 13:47:27 +0200

    Fix tests

--- web/tests/test_api.py
+++ web/tests/test_api.py
@@ -6,47 +6,53 @@ from django.core import mail
 
 from web.models import Resource
 
+
 @pytest.mark.client
 @pytest.mark.django_db
 def test_submission_event_online(rf, client, db, normal_user):
-    data = {'email': 'mike.jones@example.com',
-            'language': 'German',
-            'contributiontype': 'event_online',
-            'is_community': True,
-            'institution': 'OEC',
-            'institutionurl': 'http://www.oeconsortium.org',
-            'directions': None,
-            'archive': True,
-            'link': 'http://oeconsortium.org/events/online/20',
-            'description': 'Description of Webinar about OER',
-            'country': 'United States',
-            'lastname': 'Jones',
-            'is_primary': False,
-            'firstname': 'Mike',
-            'license': None,
-            'datetime': '2017-03-27T19:00:00+02:00',
-            'is_higher': True,
-            'title': 'Webinar about OER',
-            'city': 'New York',
-            'localeventtype': 'webinar'
+    data = {
+        'data': {
+            'attributes': {'email': 'mike.jones@example.com',
+                           'language': 'German',
+                           'contributiontype': 'event',
+                           'eventtype': 'online',
+                           'is_community': True,
+                           'institution': 'OEC',
+                           'institutionurl': 'http://www.oeconsortium.org',
+                           'directions': None,
+                           'archive': True,
+                           'link': 'http://oeconsortium.org/events/online/20',
+                           'description': 'Description of Webinar about OER',
+                           'country': 'United States',
+                           'lastname': 'Jones',
+                           'is_primary': False,
+                           'firstname': 'Mike',
+                           'license': None,
+                           'datetime': '2018-03-27T19:00:00+02:00',
+                           'is_higher': True,
+                           'title': 'Webinar about OER',
+                           'city': 'New York',
+                           },
+            'type': 'submission'
         }
+    }
 
-    client.login(username='user', password='password')
-    response = client.post('/api/submission/', content_type='application/json', data=json.dumps(data))
-    assert data.get('title') in str(response.content), 'Remote Event Submission failed'
+    # client.login(username='user', password='password')
+    response = client.post('/api/submission', content_type='application/vnd.api+json', data=json.dumps(data))
+    assert data.get('data').get('attributes').get('title') in str(response.content), 'Webinar about OER'
 
     resource = Resource.objects.latest('id')
-    assert resource.title == data.get('title')
-    assert resource.content == data.get('description')
-    assert resource.event_type == 'webinar'
+    assert resource.title == data.get('data').get('attributes').get('title')
+    assert resource.content == data.get('data').get('attributes').get('description')
+    assert resource.event_type == 'online'
 
 
 @pytest.mark.client
 @pytest.mark.django_db
 def test_submission_event_local(rf, client, db, normal_user):
     data = {
-        'contributiontype': 'event_local',
-        'localeventtype': 'workshop',
+        'contributiontype': 'event',
+        'eventtype': 'local',
 
         'firstname': 'Mike',
         'lastname': 'Jones',
@@ -60,29 +66,30 @@ def test_submission_event_local(rf, client, db, normal_user):
         'link': 'http://www.oeconsoritum.org/event/14',
         'license': None,
 
-        'datetime': '2017-04-12T15:30:00+02:00',
+        'datetime': '2018-04-12T15:30:00+02:00',
         'directions': 'Main Street 5, Baltimore',
-
 
         'title': 'Creative Destruction:  An ‘Open Textbook’ disrupting personal and institutional praxis',
         'archive': True,
 
-
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,'
+                       'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim'
+                       'veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo '
+                       'consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum'
+                       'dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident',
         'language': 'English',
-
         'is_primary': False,
         'is_higher': True,
         'is_community': True,
     }
 
     client.login(username='user', password='password')
-    response = client.post('/api/submission/', content_type='application/json', data=json.dumps(data))
+    response = client.post('/api/submission', content_type='application/json', data=json.dumps(data))
     assert data.get('datetime') in str(response.content), 'Local Event Submission failed'
 
     resource = Resource.objects.latest('id')
     assert resource.title == data.get('title')
-    assert resource.event_type == 'workshop'
+    assert resource.event_type == 'local'
 
 
 @pytest.mark.client
@@ -94,7 +101,7 @@ def test_submission_oer_resource(rf, client, db, normal_user):
         'firstname': 'Mike',
         'lastname': 'Smith',
         'title': 'Building for Scale: Updates to Google Maps APIs Standard Plan',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
         'localeventtype': None,
         'is_community': True,
         'country': 'United States',
@@ -106,13 +113,13 @@ def test_submission_oer_resource(rf, client, db, normal_user):
         'is_higher': True,
         'institutionurl': 'http://www.oeconsortium.org',
         'language': 'English',
-        'datetime': '2017-03-27T00:00:00+02:00',
+        'datetime': '2018-03-27T00:00:00+02:00',
         'license': 'CC-BY-SA',
         'directions': None
     }
 
     client.login(username='user', password='password')
-    response = client.post('/api/submission/', content_type='application/json', data=json.dumps(data))
+    response = client.post('/api/submission', content_type='application/json', data=json.dumps(data))
     assert data.get('title') in str(response.content), 'OER Resource Submission failed'
 
     resource = Resource.objects.latest('id')
@@ -125,7 +132,13 @@ def test_submission_oer_resource(rf, client, db, normal_user):
 def test_submission_project(rf, client, db, normal_user):
     data = {
         'archive': False,
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod'
+                       '\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim '
+                       'veniam,\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea'
+                       'commodo\nconsequat. Duis aute irure dolor in reprehenderit in voluptate '
+                       'velit esse\ncillum dolore eu fugiat nulla pariatur. Excepteur sint '
+                       'occaecat cupidatat non\nproident, sunt in culpa qui officia deserunt mollit'
+                       'anim id est laborum.',
         'city': 'San Francisco, CA',
         'lastname': 'Smith',
         'link': 'http://www.oeconsortium.org/project/showcase/',
@@ -133,7 +146,7 @@ def test_submission_project(rf, client, db, normal_user):
         'institutionurl': 'http://www.oeconsortium.org/',
         'language': 'English',
         'is_higher': False,
-        'datetime': '2017-03-27T00:00:00+02:00',
+        'datetime': '2018-03-27T00:00:00+02:00',
         'license': 'Freely accessible',
         'institution': 'OEC',
         'localeventtype': None,
@@ -147,7 +160,7 @@ def test_submission_project(rf, client, db, normal_user):
     }
 
     client.login(username='user', password='password')
-    response = client.post('/api/submission/', content_type='application/json', data=json.dumps(data))
+    response = client.post('/api/submission', content_type='application/json', data=json.dumps(data))
     assert data.get('title') in str(response.content), 'OER Resource Submission failed'
 
     resource = Resource.objects.latest('id')
@@ -172,17 +185,18 @@ def test_submission_online_event_other(rf, client, db, normal_user):
         'country': 'United Kingdom',
         'email': 'mike2@example.com',
         'firstname': 'Mike',
-        'title': 'Car info limit field length to 50 chars',
-        'datetime': '2017-03-27T00:00:00+02:00',
+        'title': 'OER Resource Submission failed',
+        'datetime': '2018-03-27T00:00:00+02:00',
         'eventother': 'Twitter Chat',
         'link': '',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod'
+                       '\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
         'is_community': False,
         'institutionurl': ''
     }
 
     client.login(username='user', password='password')
-    response = client.post('/api/submission/', content_type='application/json', data=json.dumps(data))
+    response = client.post('/api/submission', content_type='application/json', data=json.dumps(data))
     assert data.get('title') in str(response.content), 'OER Resource Submission failed'
 
     resource = Resource.objects.latest('id')
@@ -207,17 +221,17 @@ def test_submission_email(rf, client, db, normal_user):
         'country': 'United Kingdom',
         'email': 'mike2@example.com',
         'firstname': 'Mike',
-        'title': 'Car info limit field length to 50 chars',
-        'datetime': '2017-03-27T00:00:00+02:00',
+        'title': 'OER Resource Submission failed',
+        'datetime': '2018-03-27T00:00:00+02:00',
         'eventother': 'Twitter Chat',
         'link': '',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
         'is_community': False,
         'institutionurl': ''
     }
 
     client.login(username='user', password='password')
-    response = client.post('/api/submission/', content_type='application/json', data=json.dumps(data))
+    response = client.post('/api/submission', content_type='application/json', data=json.dumps(data))
     assert data.get('title') in str(response.content), 'OER Resource Submission failed'
 
     assert len(mail.outbox) == 1
