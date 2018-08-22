8737b1f454d45d3a9c897a6502e1f7e701617863
Jure Cuhalev <jure@cuhalev.com>
2018-08-22 16:28:48 +0200

    Remove unused import

--- web/utils.py
+++ web/utils.py
@@ -1,6 +1,4 @@
 from rest_framework_jwt.utils import jwt_payload_handler
-from django.contrib.auth.models import User
-
 
 def custom_jwt_payload_handler(user):
     payload = jwt_payload_handler(user)
