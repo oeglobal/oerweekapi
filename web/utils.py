bd542ff9b8dcaf55beed3d0070bab92cea0e7872
Jure Cuhalev <jure@cuhalev.com>
2018-09-04 11:35:49 +0200

    Only log form submission validations

--- web/utils.py
+++ web/utils.py
@@ -2,6 +2,7 @@ from rest_framework_jwt.utils import jwt_payload_handler
 from rest_framework_json_api.exceptions import exception_handler
 from raven.contrib.django.raven_compat.models import client
 
+from web.serializers import SubmissionResourceSerializer
 
 def custom_jwt_payload_handler(user):
     payload = jwt_payload_handler(user)
@@ -12,6 +13,8 @@ def custom_jwt_payload_handler(user):
 
 def custom_drf_exception_handler(exc, context):
     response = exception_handler(exc, context)
-    client.captureMessage('Form Submission Validation Error', level='debug', extra=exc.get_full_details())
+
+    if isinstance(context.get('view').get_serializer_class(), SubmissionResourceSerializer):
+        client.captureMessage('Form Submission Validation Error', level='debug', extra=exc.get_full_details())
 
     return response
