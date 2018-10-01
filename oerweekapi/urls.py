9db8d91e4deb280d0f6ec8c10c9922cc3fe17815
Jure Cuhalev <jure@cuhalev.com>
2018-10-01 17:39:31 +0200

    Make linter happy

--- oerweekapi/urls.py
+++ oerweekapi/urls.py
@@ -20,16 +20,16 @@ router.register(r'submission', SubmissionViewSet, base_name='Submission')
 router.register(r'email-templates', EmailTemplateView)
 
 urlpatterns = [
-                  url(r'^api/', include(router.urls)),
-                  # url(r'^api/submission/', SubmissionView.as_view()),
-                  url(r'^api/events-summary/', EventSummaryView.as_view()),
-                  url(r'^api/twitter/', TwitterSearchResults.as_view()),
-                  url(r'^api/request-access/', RequestAccessView.as_view()),
+    url(r'^api/', include(router.urls)),
+    # url(r'^api/submission/', SubmissionView.as_view()),
+    url(r'^api/events-summary/', EventSummaryView.as_view()),
+    url(r'^api/twitter/', TwitterSearchResults.as_view()),
+    url(r'^api/request-access/', RequestAccessView.as_view()),
 
-                  url(r'^admin/', admin.site.urls),
-                  url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-                  url(r'^api-token-auth/', rest_framework_jwt.views.obtain_jwt_token),
-                  url(r'^api-token-refresh/', rest_framework_jwt.views.refresh_jwt_token),
+    url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', rest_framework_jwt.views.obtain_jwt_token),
+    url(r'^api-token-refresh/', rest_framework_jwt.views.refresh_jwt_token),
 
-                  url(r'^export/resources/$', ExportResources.as_view(), name='resource_export'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^export/resources/$', ExportResources.as_view(), name='resource_export'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
