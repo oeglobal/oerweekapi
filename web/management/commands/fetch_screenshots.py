890a19ab5e01e8777833bbad4dcfe60a8342a8ff
Jure Cuhalev <jure@cuhalev.com>
2018-11-07 14:27:23 +0100

    Only fetch screenshots of current year

--- web/management/commands/fetch_screenshots.py
+++ web/management/commands/fetch_screenshots.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
 from web.models import Resource
 
 
@@ -6,6 +7,6 @@ class Command(BaseCommand):
     help = "Fetches screenshots"
 
     def handle(self, *args, **options):
-        for resource in Resource.objects.filter(screenshot_status__in=['', 'PENDING'], year=2018, post_status__in=['draft', 'publish']):
+        for resource in Resource.objects.filter(screenshot_status__in=['', 'PENDING'], year=settings.OEW_YEAR, post_status__in=['draft', 'publish']):
             print(resource.id)
             resource.get_screenshot()
