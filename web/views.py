546c6d37d21f3b8a82ebdc833a74b27a237c1d8b
Jure Cuhalev <jure@cuhalev.com>
2019-02-23 11:22:14 -0600

    Fix export year

--- web/views.py
+++ web/views.py
@@ -216,7 +216,7 @@ class ExportResources(LoginRequiredMixin, View):
         font_style = xlwt.XFStyle()
         font_style.alignment.wrap = 1
 
-        for resource in Resource.objects.filter(post_status='publish', year=2018):
+        for resource in Resource.objects.filter(post_status='publish', year=2019):
             row_num += 1
 
             event_time = ''
