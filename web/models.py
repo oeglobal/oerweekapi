cce347090aaa84d682822167548055541be68acd
Jure Cuhalev <jure@cuhalev.com>
2021-01-15 10:41:22 +0100

    Adds anytime event type

--- web/models.py
+++ web/models.py
@@ -23,13 +23,15 @@ User = get_user_model()
 
 class ReviewModel(models.Model):
     REVIEW_CHOICES = Choices(
-        ('new', 'New Entry'),
-        ('feedback', 'Requested feedback'),
-        ('approved', 'Approved'),
-        ('rejected', 'Rejected')
+        ("new", "New Entry"),
+        ("feedback", "Requested feedback"),
+        ("approved", "Approved"),
+        ("rejected", "Rejected"),
     )
-    status = models.CharField(choices=REVIEW_CHOICES, default='new', max_length=16)
-    reviewer = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE)
+    status = models.CharField(choices=REVIEW_CHOICES, default="new", max_length=16)
+    reviewer = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE
+    )
 
     class Meta:
         abstract = True
@@ -46,6 +48,7 @@ class Page(TimeStampedModel):
 
 class Category(TimeStampedModel):
     """Wordpress Category Taxonomy"""
+
     wp_id = models.IntegerField()
     name = models.CharField(max_length=255)
     slug = models.CharField(max_length=255)
@@ -53,30 +56,26 @@ class Category(TimeStampedModel):
 
 class Resource(TimeStampedModel, ReviewModel):
     RESOURCE_TYPES = Choices(
-        ('resource', 'Resource'),
-        ('project', 'Project'),
-        ('event', 'Event')
+        ("resource", "Resource"), ("project", "Project"), ("event", "Event")
     )
 
     POST_STATUS_TYPES = Choices(
-        ('publish', 'Publish'),
-        ('draft', 'Draft'),
-        ('trash', 'Trash')
+        ("publish", "Publish"), ("draft", "Draft"), ("trash", "Trash")
     )
 
     EVENT_TYPES = Choices(
         # duplicated due to legacy choices
-        ('conference/forum/discussion', 'Conference/forum/discussion'),
-        ('conference/seminar', 'Conference/seminar'),
-        ('workshop', 'Workshop'),
-        ('forum/panel/discussion', 'Forum/panel/discussion'),
-        ('other_local', 'other_local'),
-        ('local', 'local'),
-
-        ('webinar', 'Webinar'),  # online
-        ('discussion', 'Online Discussion'),  # online
-        ('other_online', 'Other - Online'),  # online
-        ('online', 'Online Event')
+        ("conference/forum/discussion", "Conference/forum/discussion"),
+        ("conference/seminar", "Conference/seminar"),
+        ("workshop", "Workshop"),
+        ("forum/panel/discussion", "Forum/panel/discussion"),
+        ("other_local", "other_local"),
+        ("local", "local"),
+        ("webinar", "Webinar"),  # online
+        ("discussion", "Online Discussion"),  # online
+        ("other_online", "Other - Online"),  # online
+        ("online", "Online Event"),
+        ("anytime", "Anytime Event"),
     )
 
     post_type = models.CharField(choices=RESOURCE_TYPES, max_length=25)
@@ -106,7 +105,9 @@ class Resource(TimeStampedModel, ReviewModel):
     country = models.CharField(max_length=255, blank=True)
 
     event_time = models.DateTimeField(blank=True, null=True)
-    event_type = models.CharField(max_length=255, blank=True, null=True, choices=EVENT_TYPES)
+    event_type = models.CharField(
+        max_length=255, blank=True, null=True, choices=EVENT_TYPES
+    )
     event_online = models.BooleanField(default=False)
     event_source_datetime = models.CharField(max_length=255, blank=True)
     event_source_timezone = models.CharField(max_length=255, blank=True)
@@ -123,24 +124,32 @@ class Resource(TimeStampedModel, ReviewModel):
 
     categories = models.ManyToManyField(Category, blank=True)
     tags = TaggableManager(blank=True)
-    opentags = ArrayField(models.CharField(max_length=255, blank=True, ), blank=True)
+    opentags = ArrayField(
+        models.CharField(
+            max_length=255,
+            blank=True,
+        ),
+        blank=True,
+    )
 
     notified = models.BooleanField(default=False)
     raw_post = models.TextField(blank=True)
 
-    screenshot_status = models.CharField(blank=True, default='', max_length=64)
+    screenshot_status = models.CharField(blank=True, default="", max_length=64)
 
     year = models.IntegerField(blank=True, null=True, default=settings.OEW_YEAR)
     oeaward = models.BooleanField(default=False)
 
-    image = models.ForeignKey('ResourceImage', null=True, default=None, blank=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(
+        "ResourceImage", null=True, default=None, blank=True, on_delete=models.CASCADE
+    )
     twitter = models.CharField(blank=True, null=True, max_length=255)
 
     def __str__(self):
-        return 'Resource #{}'.format(self.id)
+        return "Resource #{}".format(self.id)
 
     def get_full_url(self):
-        if self.post_type == 'event':
+        if self.post_type == "event":
             return "http://www.openeducationweek.org/events/{}".format(self.slug)
 
         return "http://www.openeducationweek.org/resources/{}".format(self.slug)
@@ -165,30 +174,40 @@ class Resource(TimeStampedModel, ReviewModel):
                 self.slug = slugify(self.title)
 
                 if next:
-                    self.slug += '-{0}'.format(next)
+                    self.slug += "-{0}".format(next)
                 next += 1
 
         if self.firstname or self.lastname:
-            self.contact = '{} {}'.format(self.firstname, self.lastname)
+            self.contact = "{} {}".format(self.firstname, self.lastname)
 
         super().save(*args, **kwargs)
 
     def get_screenshot(self):
         def webshrinker_v2(access_key, secret_key, url, params):
-            params['key'] = access_key
-            request = "thumbnails/v2/{}?{}".format(urlsafe_b64encode(url.encode()).decode(), urlencode(params, True))
-            signed_request = hashlib.md5("{}:{}".format(secret_key, request).encode('utf-8')).hexdigest()
+            params["key"] = access_key
+            request = "thumbnails/v2/{}?{}".format(
+                urlsafe_b64encode(url.encode()).decode(), urlencode(params, True)
+            )
+            signed_request = hashlib.md5(
+                "{}:{}".format(secret_key, request).encode("utf-8")
+            ).hexdigest()
 
-            return "https://api.webshrinker.com/{}&hash={}".format(request, signed_request)
+            return "https://api.webshrinker.com/{}&hash={}".format(
+                request, signed_request
+            )
 
         if self.image:
-            self.screenshot_status = 'DONE'
+            self.screenshot_status = "DONE"
             return self.save()
         print(self.link)
 
-        if self.link and self.screenshot_status in ['', 'PENDING']:
-            api_url = webshrinker_v2(settings.WEBSHRINKER_KEY, settings.WEBSHRINKEY_SECRET, self.link,
-                                     {'size': '3xlarge'})
+        if self.link and self.screenshot_status in ["", "PENDING"]:
+            api_url = webshrinker_v2(
+                settings.WEBSHRINKER_KEY,
+                settings.WEBSHRINKEY_SECRET,
+                self.link,
+                {"size": "3xlarge"},
+            )
             print(api_url)
             response = requests.get(api_url)
 
@@ -196,23 +215,28 @@ class Resource(TimeStampedModel, ReviewModel):
 
             if status_code == 200:
                 resource_image = ResourceImage()
-                resource_image.image.save('screenshot_{}.png'.format(self.pk), ContentFile(response.content))
+                resource_image.image.save(
+                    "screenshot_{}.png".format(self.pk), ContentFile(response.content)
+                )
                 resource_image.save()
 
                 self.image = resource_image
-                self.screenshot_status = 'DONE'
+                self.screenshot_status = "DONE"
                 self.save()
             elif status_code == 202:
-                self.screenshot_status = 'PENDING'
+                self.screenshot_status = "PENDING"
                 self.save()
             else:
-                print('Status code {}'.format(response.status_code))
+                print("Status code {}".format(response.status_code))
                 raise NotImplementedError
 
     def send_new_submission_email(self):
-        send_mail('emails/submission_received.tpl', {},
-                  'info@openeducationweek.org', [self.email]
-                  )
+        send_mail(
+            "emails/submission_received.tpl",
+            {},
+            "info@openeducationweek.org",
+            [self.email],
+        )
 
     def send_new_account_email(self, force=False):
         email = self.email.lower()
@@ -221,31 +245,37 @@ class Resource(TimeStampedModel, ReviewModel):
                 username=email,
                 email=email,
                 defaults={
-                    'first_name': self.firstname[:30],
-                    'last_name': self.lastname[:30],
-                    'is_active': True
-                })
+                    "first_name": self.firstname[:30],
+                    "last_name": self.lastname[:30],
+                    "is_active": True,
+                },
+            )
             key = uuid.uuid4().hex
             user.set_password(key)
             user.save()
 
-            send_mail('emails/account_created.tpl', {'user': user, 'key': key},
-                      'info@openeducationweek.org', [self.email]
-                      )
+            send_mail(
+                "emails/account_created.tpl",
+                {"user": user, "key": key},
+                "info@openeducationweek.org",
+                [self.email],
+            )
 
 
 class EmailTemplate(models.Model):
     name = models.CharField(max_length=128)
     subject = models.CharField(max_length=255)
-    body = models.TextField(help_text='You can use the following variables in body and title: ' +
-                                      '{{title}}, {{name}}, {{link}}. HTML is not allowed.')
+    body = models.TextField(
+        help_text="You can use the following variables in body and title: "
+        + "{{title}}, {{name}}, {{link}}. HTML is not allowed."
+    )
 
     def __str__(self):
         return self.name
 
 
 class ResourceImage(models.Model):
-    image = models.ImageField(upload_to='images/', blank=True)
+    image = models.ImageField(upload_to="images/", blank=True)
 
     def __str__(self):
         return repr(self.image)
